7391afda8567f3ba923f5ad0f40dbc2e2f57f13e
Mit Kamal Sheth <sheth.mit@northeastern.edu>
2024-01-12 12:22:01 -0500

    Initial Commit

---
+++ Guess The Flag/Guess_The_FlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_The_FlagApp.swift
+//  Guess The Flag
+//
+//  Created by Mit Sheth on 1/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Guess_The_FlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
